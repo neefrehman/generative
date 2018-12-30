288c963e00bdb89d91b347fbc98a71338a3f4a71
Neef Rehman <neef.rehman@gmail.com>
2018-12-30 18:15:06 +0000

    010118->010119

--- sketches/010119.js
+++ sketches/010119.js
@@ -7,7 +7,7 @@ function draw() {
     fill(255);
   } else {
     noStroke();
-    fill(Math.floor((Math.random()*255)), Math.floor((Math.random()*255)), Math.floor((Math.random()*255)), 75);
+    fill(Math.floor((Math.random()*255)), Math.floor((Math.random()*255)), Math.floor((Math.random()*255)), 80);
   }
   ellipse(mouseX, mouseY, (Math.random()*100), (Math.random()*100));
 }

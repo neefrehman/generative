70e4f7c3b2a5f85bf5e45ff5453cb6be28842c6b
Neef Rehman <neef.rehman@gmail.com>
2018-12-30 21:51:08 +0000

    index changes and sketch edits

--- sketches/010119.js
+++ sketches/010119.js
@@ -4,10 +4,12 @@ function setup() {
 
 function draw() {
   if (mouseIsPressed) {
-    fill(255);
+    stroke(0);
+    fill(Math.floor((Math.random()*255)), Math.floor((Math.random()*255)), Math.floor((Math.random()*255)), 80);
   } else {
     noStroke();
     fill(Math.floor((Math.random()*255)), Math.floor((Math.random()*255)), Math.floor((Math.random()*255)), 80);
   }
   ellipse(mouseX, mouseY, (Math.random()*100), (Math.random()*100));
+  frameRate(30);
 }

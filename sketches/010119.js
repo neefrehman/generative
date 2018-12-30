c4839600abf0fccb24315b9f61b2b56c60d7a4c7
Neef Rehman <neef.rehman@gmail.com>
2018-12-30 22:18:37 +0000

    click border -> click full opacity

--- sketches/010119.js
+++ sketches/010119.js
@@ -4,8 +4,8 @@ function setup() {
 
 function draw() {
   if (mouseIsPressed) {
-    stroke(0);
-    fill(Math.floor((Math.random()*255)), Math.floor((Math.random()*255)), Math.floor((Math.random()*255)), 80);
+    noStroke();
+    fill(Math.floor((Math.random()*255)), Math.floor((Math.random()*255)), Math.floor((Math.random()*255)));
   } else {
     noStroke();
     fill(Math.floor((Math.random()*255)), Math.floor((Math.random()*255)), Math.floor((Math.random()*255)), 80);

3b505a6b22433a124e3df0813b822df6e6bbad66
Neef Rehman <neef.rehman@gmail.com>
2019-01-04 20:38:04 +0000

    Add 050119

---
+++ sketches/050119.js
@@ -0,0 +1,140 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+}
+
+function draw() {
+
+  const x1 = (Math.random() * 200);
+  const y1 = (Math.random() * 200);
+  const x2 = (Math.random() * 200);
+  const y2 = (Math.random() * 200);
+  const x3 = (Math.random() * 200);
+  const y3 = (Math.random() * 200);
+  const x4 = (Math.random() * 200);
+  const y4 = (Math.random() * 200);
+
+  noStroke();
+  frameRate(10);
+  fill((Math.random() * 255), 200);
+
+  translate(-windowWidth/2, -windowHeight/2);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(-windowWidth, 200);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(-windowWidth, 200);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(-windowWidth, 200);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(-windowWidth, 200);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+  translate(200, 0);
+  quad(x1, y1, x2, y2, x3, y3, x4, y4);
+
+}

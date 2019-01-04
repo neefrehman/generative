fc64e1e0ab5cdf60b3c68181127f001fc6dabd25
Neef Rehman <neef.rehman@gmail.com>
2019-01-04 18:37:26 +0000

    directional light change

--- sketches/040119.js
+++ sketches/040119.js
@@ -11,7 +11,7 @@ function draw() {
 
   var dirY = (mouseY / height - 0.5) *2;
   var dirX = (mouseX / width - 0.5) *2;
-  directionalLight(250, 250, 250, dirX, -dirY, 0.25);
+  directionalLight(250, 250, 250, -dirX, -dirY, 0.25);
   specularMaterial(250, 250, 250);
 
 }

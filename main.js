3874eb416189584f861e42a1fcb97f0919a04743
Neef Rehman <neef.rehman@gmail.com>
2019-01-04 19:29:19 +0000

    SPA prep

--- main.js
+++ main.js
@@ -3,10 +3,28 @@ const darkMode = () => document.documentElement.classList.toggle("dark");
 
 darkModeToggle.addEventListener("dblclick", e => {
     darkMode();
-    
+
     if (document.documentElement.classList.contains("dark")) {
         localStorage.setItem("darkMode", true);
     } else {
         localStorage.removeItem("darkMode");
     }
 });
+
+
+// // SPA
+// const sketchLinks = document.querySelectorAll(".sketchlink");
+// const codeLink = document.querySelector(".code");
+//
+// sketchLinks.forEach(link => {
+//
+//     link.addEventListener("click", () => {
+//         const sketchName = link.innerHTML;
+//         history.replaceState(stateObj, `${sketchName} - Generative - Neef Rehman`, `${sketchName}.html`);
+//         codeLink.innerHTML = sketchName;
+//         // Show Footer
+//         // Hide body & add .sketch
+//         // Load in script
+//     });
+//
+// });

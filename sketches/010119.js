c1fabd396514dfd2c5c63b99d3b0a3c8846c2a7f
Neef Rehman <neef.rehman@gmail.com>
2019-01-04 15:43:23 +0000

    re-order and tidy

--- sketches/010119.js
+++ sketches/010119.js
@@ -3,8 +3,12 @@ function setup() {
 }
 
 function draw() {
+  
   noStroke();
   fill(Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), 80);
+  ellipse(mouseX, mouseY, (Math.random() * 90), (Math.random() * 90));
+  frameRate(30);
+
   if (mouseIsPressed) {
     noStroke();
     fill(Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255));
@@ -12,6 +16,5 @@ function draw() {
     noStroke();
     fill(255, 0);
   }
-  ellipse(mouseX, mouseY, (Math.random() * 90), (Math.random() * 90));
-  frameRate(30);
+
 }

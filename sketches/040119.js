398e7ba87dcf85d58e25842d8a9276f300852a87
Neef Rehman <neef.rehman@gmail.com>
2019-01-04 18:35:37 +0000

    add 040119

---
+++ sketches/040119.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+}
+
+function draw() {
+
+  background(255);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  torus(100, 25, 48, 32);
+
+  var dirY = (mouseY / height - 0.5) *2;
+  var dirX = (mouseX / width - 0.5) *2;
+  directionalLight(250, 250, 250, dirX, -dirY, 0.25);
+  specularMaterial(250, 250, 250);
+
+}

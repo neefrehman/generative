38082f5d1ef39c7b5d34bf84f3ee23f85c68a4dd
Neef Rehman <neef.rehman@gmail.com>
2019-01-06 15:10:24 +0000

    07 changes

--- sketches/070119.js
+++ sketches/070119.js
@@ -1,12 +1,13 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  frameRate(30);
 }
 
 function draw() {
 
-  const posX = mouseX + (150 * Math.random()) - 150 * Math.random();
-  const posY = mouseY + (150 * Math.random()) - 150 * Math.random();
-  const col = 255 * Math.random();
+  const posX = mouseX + (200 * Math.random()) - 200 * Math.random();
+  const posY = mouseY + (200 * Math.random()) - 200 * Math.random();
+  const col = mouseX / width * (255 * Math.random());
 
   noStroke();
   fill(col, 100);

c3f9989f49b79e7d6b00ace664bad9a92a9a3c9b
Neef Rehman <neef.rehman@gmail.com>
2019-01-09 20:29:31 +0000

    undo attempt 4

--- main.js
+++ main.js
@@ -64,12 +64,12 @@ sketchLinks.forEach(link => {
 homeLink.addEventListener("click", () => goHome());
 
 window.addEventListener("popstate", () => {
-    // const newUrlPath = () => location.pathname.split("/").filter((v) => v !== "");
-    // const newLinkedSketch = newUrlPath()[newUrlPath().length - 1];
+    const newUrlPath = () => location.pathname.split("/").filter((v) => v !== "");
+    const newLinkedSketch = newUrlPath()[newUrlPath().length - 1];
 
-    if (urlPath.length == 0) {
+    if (newUrlPath.length == 0) {
         goHome();
     } else {
-        goToSketch(linkedSketch);
+        goToSketch(newLinkedSketch);
     }
   });

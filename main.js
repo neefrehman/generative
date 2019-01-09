b647ff236581b84114381b65543c22b98eb9f8f7
Neef Rehman <neef.rehman@gmail.com>
2019-01-09 15:54:13 +0000

    scoping

--- main.js
+++ main.js
@@ -64,9 +64,8 @@ const getURLPath = () => location.pathname.split('/').filter((v) => v !== '');
         } else if (linkedSketch.length >= 1) {
             const sketchId = linkedSketch[linkedSketch.length - 1];
             console.log(sketchId);
+            
+            const linkedSketchButton = document.querySelector(`#${sketchId}`);
+            linkedSketchButton.click();
         }
-
-    const linkedSketchButton = document.querySelector(`#${sketchId}`);
-
-    linkedSketchButton.click();
 // }

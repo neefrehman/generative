37f942062f38e21bf6ce70e59a0249fa7cdf7d40
Neef Rehman <neef.rehman@gmail.com>
2019-01-09 20:06:16 +0000

    404 fix

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ if (urlPath().length >= 1 && location.protocol != "file:") {
     if (sketchButton) {
         goToSketch(linkedSketch);
     } else {
-        window.location.href = "/404";
+        if (location.href.split(location.host)[1] != "/404") window.location.href = "/404";
     }
 }
 

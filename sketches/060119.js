1a5a7719a069c86c7e1b3322eb4db0480b036373
Neef Rehman <neef.rehman@gmail.com>
2019-01-11 16:52:11 +0000

    lat -> var in sketch

--- sketches/060119.js
+++ sketches/060119.js
@@ -3,14 +3,14 @@ function setup() {
   frameRate(10);
 }
 
-let x1 = (Math.random() * 100);
-let y1 = (Math.random() * 100);
-let x2 = (Math.random() * 100);
-let y2 = (Math.random() * 100);
-let x3 = (Math.random() * 100);
-let y3 = (Math.random() * 100);
-let x4 = (Math.random() * 100);
-let y4 = (Math.random() * 100);
+var x1 = (Math.random() * 100);
+var y1 = (Math.random() * 100);
+var x2 = (Math.random() * 100);
+var y2 = (Math.random() * 100);
+var x3 = (Math.random() * 100);
+var y3 = (Math.random() * 100);
+var x4 = (Math.random() * 100);
+var y4 = (Math.random() * 100);
 
 function draw() {
 

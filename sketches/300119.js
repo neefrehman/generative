102f1b0afaa65ba38393718dc0c4c8e4bfd28458
Neef Rehman <neef.rehman@gmail.com>
2019-01-30 16:40:54 +0000

    lava lamp

--- sketches/300119.js
+++ sketches/300119.js
@@ -3,14 +3,14 @@ var Slice = class Slice {
 
   constructor(x, y, r) {
     this.x = x || random(width);
-    this.y = y || height;
+    this.y = y || random((height / 2 + 100), (height / 2 - 100));
     this.r = r || 30;
-    this.speed = 4;
+    this.speed = 4 - (this.r / 10);
     this.growth = (width > 450) ? 6 : 3;
   }
 
   move() {
-    this.y = this.y - random(this.speed);
+    this.y = this.y - random(this.speed * 2);
     this.r = this.r + random(-this.growth, this.growth);
   }
 
@@ -32,7 +32,7 @@ var Slice = class Slice {
   }
 
   normal() {
-    this.speed = 4;
+    this.speed = 4 - (this.r / 10);
     this.growth = (width > 450) ? 4 : 2;
   }
 
@@ -42,9 +42,9 @@ function setup() {
 
   createCanvas(windowWidth, windowHeight);
   background(20);
-  frameRate(20);
+  frameRate(24);
 
-  const initialSliceCount = Math.floor(width / 25);
+  const initialSliceCount = Math.floor(width / 8);
   for (var i = 0; i < initialSliceCount; i++) {
     slices[i] = new Slice();
   }
@@ -52,6 +52,8 @@ function setup() {
 }
 
 function draw() {
+
+  background(20);
 
   for (const slice of slices) {
 
@@ -66,6 +68,8 @@ function draw() {
 
     if (slice.y < -slice.r) {
       slice.y = height + slice.r;
+    } else if (slice.y - slice.r > height) {
+      slice.y = 0 - slice.r;
     }
 
   }

d19f5abbc92ed0a43a79d15b4c6474efffd58269
Neef Rehman <neef.rehman@gmail.com>
2019-04-09 19:29:42 +0100

    remove IIFE

--- sketches/19/01/010119.js
+++ sketches/19/01/010119.js
@@ -1,29 +1,26 @@
-var sketch = (() => {
-
-    setup = () => {
-        createCanvas(windowWidth, windowHeight);
-        frameRate(30);
-        noStroke();
-        noFill();
-    };
+setup = () => {
+    createCanvas(windowWidth, windowHeight);
+    frameRate(30);
+    noStroke();
+    noFill();
+};
 
 
-    draw = () => {
+draw = () => {
 
-        ellipse(mouseX, mouseY, (Math.random() * 90), (Math.random() * 90));
-        fill(Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), 80);
+    ellipse(mouseX, mouseY, (Math.random() * 90), (Math.random() * 90));
+    fill(Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), 80);
 
-        if (mouseIsPressed) {
-            fill(Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255));
-        } if (mouseX == pmouseX && mouseY == pmouseY) {
-            fill(255, 0);
-        } if (windowWidth < 550) {
-            ellipse(mouseX, mouseY, (Math.random() * 70), (Math.random() * 70));
-            frameRate(20);
-        }
+    if (mouseIsPressed) {
+        fill(Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255));
+    } if (mouseX == pmouseX && mouseY == pmouseY) {
+        fill(255, 0);
+    } if (windowWidth < 550) {
+        ellipse(mouseX, mouseY, (Math.random() * 70), (Math.random() * 70));
+        frameRate(20);
+    }
 
-    };
+};
 
-})();
 
 new p5();

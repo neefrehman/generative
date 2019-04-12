8008d3fd6ca3861f2f90265bda96ef4c91c937dc
Neef Rehman <neef.rehman@gmail.com>
2019-04-12 21:52:40 +0100

    p5 0.7.3 -> 0.8.0

--- sketches/19/01/040119.js
+++ sketches/19/01/040119.js
@@ -11,14 +11,14 @@
         rotateX(frameCount * 0.01);
         rotateY(frameCount * 0.01);
 
-        torus(100, 25, 48, 32);
-        sphere(25, 48);
-
         var dirY = (mouseY / height - 0.5) * 2;
         var dirX = (mouseX / width - 0.5) * 2;
         directionalLight(255, 255, 255, -dirX, -dirY, 0.25);
         specularMaterial(255, 255, 255);
 
+        torus(100, 25, 48, 32);
+        sphere(25, 48);
+        
     };
 
 })(); new p5();

cca52d234e577a87ecb17440901b80c5e059ee21
Neef Rehman <neef.rehman@gmail.com>
2019-04-13 14:47:09 +0100

    cdn script in if statement

--- main.js
+++ main.js
@@ -1,112 +1,109 @@
-(() => {
+const allPages = document.querySelectorAll(".page");
+const homePage = document.querySelector(".home-page");
+const sketchPage = document.querySelector(".sketch-page");
+const fourOhFourPage = document.querySelector(".fourohfour-page");
 
-    const allPages = document.querySelectorAll(".page");
-    const homePage = document.querySelector(".home-page");
-    const sketchPage = document.querySelector(".sketch-page");
-    const fourOhFourPage = document.querySelector(".fourohfour-page");
+const sketchLinks = document.querySelectorAll(".sketch-link");
+const homeLinks = document.querySelectorAll(".home-link");
+const codeLink = document.querySelector(".code-link");
 
-    const sketchLinks = document.querySelectorAll(".sketch-link");
-    const homeLinks = document.querySelectorAll(".home-link");
-    const codeLink = document.querySelector(".code-link");
-
-    let sketchScript;
+let sketchScript;
 
 
-    const removeSketch = () => {
-        while (document.querySelector("canvas")) {
-            remove(); // p5
-            sketchScript.remove();
-        }
-    };
+const removeSketch = () => {
+    while (document.querySelector("canvas")) {
+        remove(); // p5
+        sketchScript.remove();
+    }
+};
 
 
-    const showPage = newPage => {
-        removeSketch();
-        allPages.forEach(page => page.classList.remove("show"));
-        newPage.classList.add("show");
-    };
+const showPage = newPage => {
+    removeSketch();
+    allPages.forEach(page => page.classList.remove("show"));
+    newPage.classList.add("show");
+};
 
 
-    const goToSketch = sketch => {
-        showPage(sketchPage);
+const goToSketch = sketch => {
+    showPage(sketchPage);
 
-        const month = sketch.substr(2, 2);
-        const year = sketch.substr(4, 2);
+    const month = sketch.substr(2, 2);
+    const year = sketch.substr(4, 2);
 
-        sketchScript = document.createElement("script");
-        sketchScript.src = `sketches/${year}/${month}/${sketch}.js`;
-        document.body.appendChild(sketchScript);
+    sketchScript = document.createElement("script");
+    sketchScript.src = `sketches/${year}/${month}/${sketch}.js`;
+    document.body.appendChild(sketchScript);
 
-        codeLink.innerHTML = sketch;
-        codeLink.href = `https://github.com/neefrehman/Generative/blob/master/sketches/${year}/${month}/${sketch}.js`;
-        document.title = `${sketch} - Generative`;
-    };
-
-
-    const goHome = () => {
-        showPage(homePage);
-
-        document.title = "Generative - Neef Rehman";
-    };
+    codeLink.innerHTML = sketch;
+    codeLink.href = `https://github.com/neefrehman/Generative/blob/master/sketches/${year}/${month}/${sketch}.js`;
+    document.title = `${sketch} - Generative`;
+};
 
 
-    const goTo404 = () => {
-        showPage(fourOhFourPage);
+const goHome = () => {
+    showPage(homePage);
 
-        document.title = "404 - Generative";
-    };
-
-
-    const getUrlPath = () => location.pathname.split("/").filter(v => v !== "");
-    let urlPath = getUrlPath();
-    let linkedSketch = urlPath[urlPath.length - 1];
+    document.title = "Generative - Neef Rehman";
+};
 
 
-    if (urlPath.length >= 1) {
-        const linkedSketchButton = document.getElementById(linkedSketch);
+const goTo404 = () => {
+    showPage(fourOhFourPage);
 
-        if (linkedSketchButton) {
-            goToSketch(linkedSketch);
-        } else {
-            goTo404();
-        }
+    document.title = "404 - Generative";
+};
+
+
+const getUrlPath = () => location.pathname.split("/").filter(v => v !== "");
+let urlPath = getUrlPath();
+let linkedSketch;
+
+
+if (urlPath.length == 0) {
+    goHome();
+} else {
+    linkedSketch = urlPath[urlPath.length - 1];
+    const linkedSketchButton = document.getElementById(linkedSketch);
+
+    if (linkedSketchButton) {
+        goToSketch(linkedSketch);
     } else {
+        goTo404();
+    }
+}
+
+
+window.addEventListener("popstate", e => {
+    urlPath = getUrlPath();
+    const newLinkedSketch = urlPath[urlPath.length - 1];
+
+    if (urlPath.length == 0 || newLinkedSketch == linkedSketch) {
         goHome();
+        history.replaceState("", document.title, "/");
+    } else {
+        goToSketch(newLinkedSketch);
     }
 
+    urlPath = getUrlPath();
+    linkedSketch = urlPath[urlPath.length - 1];
+});
 
-    window.addEventListener("popstate", e => {
-        urlPath = getUrlPath();
-        const newLinkedSketch = urlPath[urlPath.length - 1];
 
-        if (urlPath.length == 0 || newLinkedSketch == linkedSketch) {
-            goHome();
-            history.replaceState("", document.title, "/");
-        } else {
-            goToSketch(newLinkedSketch);
-        }
-
-        urlPath = getUrlPath();
-        linkedSketch = urlPath[urlPath.length - 1];
+sketchLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        goToSketch(link.id);
+        history.pushState("", `${link.id} - Generative`, link.id);
     });
+});
 
 
-    sketchLinks.forEach(link => {
-        link.addEventListener("click", () => {
-            goToSketch(link.id);
-            history.pushState("", `${link.id} - Generative`, link.id);
-        });
+homeLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        goHome();
+        history.pushState("", document.title, "/");
     });
-
-
-    homeLinks.forEach(link => {
-        link.addEventListener("click", () => {
-            goHome();
-            history.pushState("", document.title, "/");
-        });
-    });
-
-})();
+});
 
 
 // p5 windowResize into global namespace

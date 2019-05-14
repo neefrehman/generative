a74cfac9fb8fafa57bceaf584e5afcb158ae67cc
Neef Rehman <neef.rehman@gmail.com>
2019-05-14 22:09:12 +0100

    + 160519

---
+++ sketches/19/05/160519.js
@@ -0,0 +1,40 @@
+(() => {
+
+    let r = Math.floor((Math.random() * 255));
+    let g = Math.floor((Math.random() * 255));
+    let b = Math.floor((Math.random() * 255));
+
+
+
+
+    setup = () => {
+        createCanvas(windowWidth, windowHeight);
+        pixelDensity(1);
+    };
+
+
+    draw = () => {
+        background(20);
+
+        loadPixels();
+
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                const index = (x + y * width) * 4;
+                pixels[index + 0] = x;
+                pixels[index + 1] = r;
+                pixels[index + 2] = y;
+                pixels[index + 3] = b;
+            }
+        }
+
+        updatePixels();
+    };
+
+    mousePressed = () => {
+        r = Math.floor((Math.random() * 255));
+        g = Math.floor((Math.random() * 255));
+        b = Math.floor((Math.random() * 255));
+    };
+
+})(); new p5();

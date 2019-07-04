866c283b59d52dd58637d1f3ba241497d6912acc
Neef Rehman <neef.rehman@gmail.com>
2019-07-04 12:14:29 +0100

    torus lighting fix

--- sketches/19/01/040119.js
+++ sketches/19/01/040119.js
@@ -2,6 +2,7 @@
 
     setup = () => {
         createCanvas(windowWidth, windowHeight, WEBGL);
+        colorMode(HSB);
     };
 
 
@@ -13,8 +14,8 @@
 
         var dirY = (mouseY / height - 0.5) * 2;
         var dirX = (mouseX / width - 0.5) * 2;
-        directionalLight(255, 255, 255, -dirX, -dirY, 0.25);
-        specularMaterial(255, 255, 255);
+        directionalLight(255, 0, 30, -dirX, -dirY, 0.5);
+        specularMaterial(255, 0, 255);
 
         torus(100, 25, 48, 32);
         sphere(25, 48);

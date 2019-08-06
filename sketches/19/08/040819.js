dff18b8bdc2d5c6bf6c24d3bc485cd624f056aed
Neef Rehman <neef.rehman@gmail.com>
2019-08-06 17:22:38 +0100

    add prettier

--- sketches/19/08/040819.js
+++ sketches/19/08/040819.js
@@ -1,28 +1,28 @@
 (() => {
-
-    let x2, y2, x3, y3, x4, y4,
+    let x2,
+        y2,
+        x3,
+        y3,
+        x4,
+        y4,
         gridSize,
         xOff = 0,
         yOff = 200;
-
-
-
 
     setup = () => {
         createCanvas(windowWidth, windowHeight);
         background(20);
         frameRate(20);
 
-        x2 = width / 2,
-        y2 = height / 2,
-        x3 = width / 2,
-        y3 = height / 2,
-        x4 = width / 2,
+        x2 = width / 2;
+        y2 = height / 2;
+        x3 = width / 2;
+        y3 = height / 2;
+        x4 = width / 2;
         y4 = height / 2;
 
-        gridSize = (width > 450) ? 200 : 120;
+        gridSize = width > 450 ? 200 : 120;
     };
-
 
     draw = () => {
         background(20);
@@ -34,9 +34,9 @@
                 x += random(1, -1);
                 y += random(1, -1);
 
-                x2 = map(noise(xOff +   0), 0, 1, 0, width) + random(-10, 10);
+                x2 = map(noise(xOff + 0), 0, 1, 0, width) + random(-10, 10);
                 y2 = map(noise(yOff + 500), 0, 1, 0, width) + random(-10, 10);
-                x3 = map(noise(xOff +  80), 0, 1, 0, width) + random(-10, 10);
+                x3 = map(noise(xOff + 80), 0, 1, 0, width) + random(-10, 10);
                 y3 = map(noise(yOff + 300), 0, 1, 0, width) + random(-10, 10);
                 x4 = map(noise(xOff + 800), 0, 1, 0, width) + random(-10, 10);
                 y4 = map(noise(yOff + 999), 0, 1, 0, width) + random(-10, 10);
@@ -51,5 +51,4 @@
         xOff += 0.01;
         yOff += 0.01;
     };
-
 })();

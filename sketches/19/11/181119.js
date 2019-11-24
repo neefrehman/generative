27f935c4db0685f4eb63798575cb91c408da2c38
Neef Rehman <neef.rehman@gmail.com>
2019-11-24 20:33:58 +0000

    blobbly refactor

--- sketches/19/11/181119.js
+++ sketches/19/11/181119.js
@@ -2,21 +2,25 @@
 // https://www.youtube.com/watch?v=ZI1dmHv3MeM&list=PLRqwX-V7Uu6ZiZxtDDRCi6uhfTH4FilpH&index=195
 
 (() => {
+    const blobs = [];
     class Blob {
         constructor(x, y, r) {
             this.pos = createVector(x, y);
             this.r = r;
             this.vel = createVector(0, 0);
+
+            this.noiseTurbulence = 0.5;
+            this.zOff = 0;
         }
 
         show() {
             push();
             translate(this.pos.x, this.pos.y);
             beginShape();
-            for (let a = 0; a < TWO_PI; a += 0.02) {
-                let xOff = map(cos(a), -1, 1, 0, noiseMax);
-                let yOff = map(sin(a), -1, 1, 0, noiseMax);
-                const offset = map(noise(xOff, yOff, zOff), 0, 1, 0, 50);
+            for (let a = 0; a < TWO_PI; a += TWO_PI / 500) {
+                let xOff = map(cos(a), -1, 1, 0, this.noiseTurbulence);
+                let yOff = map(sin(a), -1, 1, 0, this.noiseTurbulence);
+                const offset = map(noise(xOff, yOff, this.zOff), 0, 1, 0, 50);
                 const r = this.r + offset;
                 const x = r * cos(a);
                 const y = r * sin(a);
@@ -25,16 +29,10 @@
             endShape(CLOSE);
             pop();
 
-            zOff += 0.01;
-            noiseMax += 0.0005;
+            this.zOff += 0.01;
+            this.noiseTurbulence += 0.0003;
         }
     }
-
-    let blob;
-
-    let zoom = 1;
-    let noiseMax = 0.5;
-    let zOff = 0;
 
     setup = () => {
         createCanvas(windowWidth, windowHeight);
@@ -43,22 +41,17 @@
         strokeWeight(2);
         noFill();
 
-        blob = new Blob(0, 0, 128);
+        blobs.push(new Blob(0, 0, 128));
     };
 
     draw = () => {
         background(20);
-
         translate(width / 2, height / 2);
-        var newZoom = 128 / blob.r;
-        zoom = lerp(zoom, newZoom, 0.1);
-        scale(zoom);
-        translate(-blob.pos.x, -blob.pos.y);
 
-        blob.show();
+        for (const blob of blobs) {
+            blob.show();
+        }
     };
 
-    mousePressed = () => {
-        noiseMax += 0.05;
-    };
+    mousePressed = () => {};
 })();

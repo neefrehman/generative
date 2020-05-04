ebc0ef6433c9ee6fef1bc6f00ec50daf501719cb
Neef Rehman <neef.rehman@gmail.com>
2020-05-04 14:00:26 +0100

    matrix docs update

--- src/sketches/utils/createNDimensionalArray.ts
+++ src/sketches/utils/createNDimensionalArray.ts
@@ -1,6 +1,6 @@
 /**
  * The returned  matrix (multidimensional array), typed recusively to ensure each
- * point contains the same type as `createNDimensionalArray`'s initialValues parameter
+ * point matches the type of `createNDimensionalArray`'s initialValues parameter.
  */
 type Matrix<T> = T[] | Matrix<T>[];
 
@@ -10,15 +10,15 @@ type Matrix<T> = T[] | Matrix<T>[];
  * Both numbers and number arrays are supported to create the matrix. If a number is
  * passed as the `dimensions` parameter, the smallest array with that number of
  * dimensions is returned. If an array is passed, the returned matrix will have
- * those exact dimensions. For example, passing in `[3, 2, 5]` will result in an
- * array that is 3x2x5 in size.
+ * those exact dimensions. For example, passing in `[3, 2, 5]` will result in a
+ * three-dimensional array that is 3x2x5 in size.
  *
  * All points within the matrix can be initialised to a specific value with the
  * optional `initialValues` parameter.
  *
  * @param dimensions - The desired dimensions of the matrix. A number or array of numbers.
  * @param initialValues - The value that each point in the matrix will initialise to. Defaults to `null`.
- * @returns A matrix of `N` dimensions, with each item initialised to the `initialValues` parameter.
+ * @returns A matrix of `N` dimensions, with each point initialised to the `initialValues` parameter.
  */
 const createNDimensionalArray = <T>(
     dimensions: number | number[],

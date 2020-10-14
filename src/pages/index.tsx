3da0d3c04ec82e518818c1331d128b9010470ca3
Neef Rehman <neef.rehman@gmail.com>
2020-10-14 19:38:49 +0100

    Nest css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,21 +17,21 @@ const StyledHomePage = styled.div`
         width: max-content;
         transform: scale(1.02, 1);
         transform-origin: left;
-    }
 
-    h1 {
-        margin-block-end: 0.5em;
-        white-space: nowrap;
-        font-size: 3.4em;
-        font-weight: 400;
-        margin-right: 1em;
+        h1 {
+            margin-block-end: 0.5em;
+            white-space: nowrap;
+            font-size: 3.4em;
+            font-weight: 400;
+            margin-right: 1em;
 
-        a {
-            text-decoration: underline;
-            background-color: initial;
+            a {
+                text-decoration: underline;
+                background-color: initial;
 
-            :hover {
-                background-color: #eee;
+                :hover {
+                    background-color: #eee;
+                }
             }
         }
     }
@@ -41,10 +41,10 @@ const StyledHomePage = styled.div`
 
         header {
             margin-bottom: 30px;
-        }
 
-        h1 {
-            font-size: 2.36em;
+            h1 {
+                font-size: 2.36em;
+            }
         }
     }
 

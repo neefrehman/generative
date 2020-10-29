65d320efe1d63ce6f9bb22ebed5c1be06c4bd3d4
Neef Rehman <neef.rehman@gmail.com>
2020-10-29 17:42:35 +0000

    bump deps and update sketch types [skip ci]

--- src/sketches/20/10/281020.tsx
+++ src/sketches/20/10/281020.tsx
@@ -1,29 +1,31 @@
 import React from "react";
 import { MeshDistortMaterial, OrbitControls } from "drei";
 import { Canvas } from "react-three-fiber";
-// import { makeFolder, useTweaks } from "use-tweaks";
+import { makeFolder } from "use-tweaks";
+
+import { useTweaksInDev } from "hooks/useTweaksInDev";
 
 const S281020 = () => {
-    // const { detail, rotating, peakHeight, level, speed, turbulence } = useTweaks(
-    //     "Mother Earth",
-    //     {
-    //         detail: { value: 4, min: 1, max: 10, step: 1 },
-    //         rotating: true,
-    //         ...makeFolder("Land", { peakHeight: { value: 0.45, min: 0, max: 1 } }),
-    //         ...makeFolder("Sea", {
-    //             speed: { value: 1.25, min: 0, max: 10 },
-    //             turbulence: { value: 0.25, min: 0, max: 1 },
-    //             level: { value: 1.04, min: 0, max: 1.5 },
-    //         }),
-    //     }
-    // );
-
-    const detail = 4;
-    const rotating = true;
-    const peakHeight = 0.45;
-    const level = 1.04;
-    const speed = 1.25;
-    const turbulence = 0.25;
+    const {
+        detail,
+        rotating,
+        peakHeight,
+        level,
+        speed,
+        turbulence,
+    }: { [value: string]: number } & { rotating: boolean } = useTweaksInDev(
+        "Mother Earth",
+        {
+            detail: { value: 4, min: 1, max: 10, step: 1 },
+            rotating: true,
+            ...makeFolder("Land", { peakHeight: { value: 0.45, min: 0, max: 1 } }),
+            ...makeFolder("Sea", {
+                speed: { value: 1.25, min: 0, max: 10 },
+                turbulence: { value: 0.25, min: 0, max: 1 },
+                level: { value: 1.04, min: 0, max: 1.5 },
+            }),
+        }
+    );
 
     return (
         <Canvas
@@ -40,8 +42,8 @@ const S281020 = () => {
             />
             <ambientLight intensity={0.5} />
             <pointLight intensity={1} position={[0, 200, 0]} />
-            <pointLight intensity={1} position={[100, 200, 100]} />
-            <pointLight intensity={0.5} position={[-100, -200, -100]} />
+            <pointLight intensity={1} position={[200, 200, 200]} />
+            <pointLight intensity={0.4} position={[-200, -200, -200]} />
 
             <mesh>
                 <icosahedronGeometry args={[2, detail]} />

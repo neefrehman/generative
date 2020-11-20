fc638d434631bec552cd043d01f205733054fb0b
Neef Rehman <neef.rehman@gmail.com>
2020-11-20 11:16:23 +0000

    update extensions and settings [skip ci]

--- src/sketches/utils/math/types.ts
+++ src/sketches/utils/math/types.ts
@@ -1,17 +1,17 @@
 /**
  * A vector point of to up to any dimension.
- * Adding the generic `D` will set the number of dimensions
+ * Adding the generic `D` will constrain the number of dimensions
  *
  * @example
  * const vector2D: Vector<2> = [1, 3]
  * const vector4D: Vector<4> = [1, 1, 3, 9]
  * const vectorAnyD: Vector = [1, 3, 9, 0,..., 4]
  */
+// Vector type discourse: https://github.com/microsoft/TypeScript/issues/26223
 export interface Vector<D extends number = any> extends Array<number> {
     0?: number;
     length: D;
 }
-// Follow: https://github.com/microsoft/TypeScript/issues/26223
 
 /**
  * A multidimensional array. Provides type-safety up to 5 dimensions.

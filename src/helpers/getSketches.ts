c57b61be7425257ddba081a6c9b195279c39eb75
Neef Rehman <neef.rehman@gmail.com>
2020-11-24 12:07:36 +0000

    Update TSdoc comments [skip ci]

--- src/helpers/getSketches.ts
+++ src/helpers/getSketches.ts
@@ -1,11 +1,15 @@
 import fs from "fs";
 import path from "path";
 
-/** Gets all finished sketches by recursively walking the year/month/day folders
- * @param nodePath An instance on nodes `path` library
- * @param nodeFs An instance on nodes `fs` library
+/** Gets all finished sketches, by recursively walking the `{year}/{month}/{day}` folders
+ *
+ * @param nodePath An instance of nodes `path` library
+ * @param nodeFs An instance of nodes `fs` library
  */
-export const getSketchArray = (nodePath: typeof path, nodeFs: typeof fs) => {
+export const getSketchArray = (
+    nodePath: typeof path,
+    nodeFs: typeof fs
+): string[] => {
     const sketchArray: string[] = [];
 
     const sketchPath = nodePath.resolve("src/sketches");
@@ -33,11 +37,15 @@ export const getSketchArray = (nodePath: typeof path, nodeFs: typeof fs) => {
     return sketchArray;
 };
 
-/** Gets all draft sketches from the _drafts folder
- * @param nodePath An instance on nodes `path` library
- * @param nodeFs An instance on nodes `fs` library
+/** Gets all draft sketches from the `_drafts` folder
+ *
+ * @param nodePath An instance of nodes `path` library
+ * @param nodeFs An instance of nodes `fs` library
  */
-export const getDraftsArray = (nodePath: typeof path, nodeFs: typeof fs) => {
+export const getDraftsArray = (
+    nodePath: typeof path,
+    nodeFs: typeof fs
+): string[] => {
     const draftsPath = nodePath.resolve("src/sketches/_drafts");
     const draftsArray = nodeFs
         .readdirSync(draftsPath)
@@ -46,11 +54,15 @@ export const getDraftsArray = (nodePath: typeof path, nodeFs: typeof fs) => {
     return draftsArray.filter(name => name !== "_archive");
 };
 
-/** Gets all archived drafts sketches from the _drafts/archive folder
- * @param nodePath An instance on nodes `path` library
- * @param nodeFs An instance on nodes `fs` library
+/** Gets all archived drafts sketches from the `_drafts/_archive` folder
+ *
+ * @param nodePath An instance of nodes `path` library
+ * @param nodeFs An instance of nodes `fs` library
  */
-export const getArchivedArray = (nodePath: typeof path, nodeFs: typeof fs) => {
+export const getArchivedArray = (
+    nodePath: typeof path,
+    nodeFs: typeof fs
+): string[] => {
     const archivePath = nodePath.resolve("src/sketches/_drafts/_archive");
     const archiveArray = nodeFs
         .readdirSync(archivePath)
--- src/helpers/isWebGL2Supported.ts
+++ src/helpers/isWebGL2Supported.ts
@@ -1,2 +1,12 @@
+/**
+ * Detects browser WebGL2 support. Useful for providing fallback messgaes for sketches
+ * 
+ * @example
+ * return isWebGL2Supported() ? (
+        <SkecthComponent />
+    ) : (
+        <TextOverlay text="Your browser doesn't support WebGL2" timeout={false} />
+    );
+ */
 export const isWebGL2Supported = (): boolean =>
     !!document.createElement("canvas").getContext("webgl2");

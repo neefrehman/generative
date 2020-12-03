274dafc492e2d0b47e674197fe88c715f7ba1c47
Neef Rehman <neef.rehman@gmail.com>
2020-12-03 16:06:37 +0000

    Remove extraneous comments [skip ci]

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,20 +14,17 @@ class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
-                    {/* General */}
                     <meta name="author" content="Neef Rehman" />
                     <link rel="icon" href="/static/favicon.ico" />
                     <meta name="title" content={title} />
                     <meta name="description" content={description} />
 
-                    {/* OG */}
                     <meta property="og:type" content="website" />
                     <meta property="og:url" content={url} />
                     <meta property="og:title" content={title} />
                     <meta property="og:description" content={description} />
                     <meta property="og:image" content={imageUrl} />
 
-                    {/* Twitter */}
                     <meta property="twitter:card" content="summary_large_image" />
                     <meta property="twitter:url" content={url} />
                     <meta property="twitter:title" content={title} />

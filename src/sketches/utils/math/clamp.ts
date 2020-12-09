a37c18402ba6cdc8829e680c660505d691526339
Neef Rehman <neef.rehman@gmail.com>
2020-12-09 12:57:53 +0000

    add clamp to utils

---
+++ src/sketches/utils/math/clamp.ts
@@ -0,0 +1,16 @@
+/**
+ * Clamps a dynamic input between minumum and maximum values
+ *
+ * @param minimum - lower bound of the clamp
+ * @param value - the incoming value to be clamped
+ * @param maximum - upper bound of the clamp
+ *
+ * @return the clamped number
+ */
+export const mapRange = (
+    minimum: number,
+    value: number,
+    maximum: number
+): number => {
+    return Math.min(Math.max(value, minimum), maximum);
+};

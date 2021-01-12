763d158754be759ddb486b11c7a4c572e8f0cfec
Neef Rehman <neef.rehman@gmail.com>
2021-01-12 16:48:08 +0000

    fix isInteger probabilities

--- src/sketches/utils/random/inRange.ts
+++ src/sketches/utils/random/inRange.ts
@@ -4,7 +4,7 @@
  * is used as the max.
  *
  * @param min - The lower bound of the range
- * @param min - The upper bound of the range
+ * @param max - The upper bound of the range
  * @param options.isInteger - A boolean than will force the returned value to be an integer if `true`
  * @param options.not - A number to *not* use as the generated value. useful when generating multiple numbers
  *
@@ -14,17 +14,20 @@ export const inRange = (
     min: number,
     max?: number,
     options?: { isInteger?: boolean; not?: number }
-) => {
+): number => {
     const { isInteger = false, not = undefined } = options ?? {};
     const upperBound = max ?? min;
     const lowerBound = max ? min : 0;
 
-    let generatedNumber =
-        Math.random() * (upperBound - lowerBound) + (max ? min : 0);
+    let generatedNumber = isInteger
+        ? Math.floor(
+              Math.random() * (Math.ceil(upperBound) - Math.floor(lowerBound) + 1)
+          ) + Math.floor(lowerBound)
+        : Math.random() * (upperBound - lowerBound) + lowerBound;
 
     if (not !== undefined && generatedNumber === not) {
         generatedNumber = inRange(min, max, options);
     }
 
-    return isInteger ? Math.round(generatedNumber) : generatedNumber;
+    return generatedNumber;
 };

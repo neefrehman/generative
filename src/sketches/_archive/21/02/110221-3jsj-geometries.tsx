a7ede29c02005a78881b95d08c5a1d1a7c07c6e7
Neef Rehman <neef.rehman@gmail.com>
2021-02-11 21:13:37 +0000

    geometry lesson

---
+++ src/sketches/_archive/21/02/110221-3jsj-geometries.tsx
@@ -0,0 +1,49 @@
+// lesson 9
+
+import React from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+import { ThreeRenderer } from "Renderers/Three";
+import type { ThreeSetupFn } from "Renderers/Three";
+
+const sketch: ThreeSetupFn = ({ scene, camera, canvas }) => {
+    camera.position.z = 5;
+    const controls = new OrbitControls(camera, canvas);
+    controls.enableDamping = true;
+
+    // const geometry = new THREE.BoxBufferGeometry(2, 2, 2, 3, 3, 3);
+    const geometry = new THREE.BufferGeometry();
+
+    /* Make a triangle: */
+    // prettier-ignore
+    // const positionsArray = new Float32Array([
+    //     0, 0, 0,
+    //     0, 1, 0,
+    //     1, 0, 0
+    // ]);
+    // normalised to three, because the array is full of 3d vectors (uv would be 2)
+    // const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+
+    /* random values */
+    const TRIANGLE_COUNT = 100;
+    const positionsArray = new Float32Array(TRIANGLE_COUNT * 3 * 3);
+    positionsArray.forEach((_, i) => {
+        positionsArray[i] = (Math.random() - 0.5) * 4;
+    });
+    const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+
+    geometry.setAttribute("position", positionsAttribute); // "position" is shader attribute
+    const material = new THREE.MeshBasicMaterial({ wireframe: true });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    return () => {
+        // keep OrbitControls damping even after the user stops pressing the mouse
+        controls.update();
+    };
+};
+
+const S311020 = () => <ThreeRenderer sketch={sketch} />;
+
+export default S311020;
